7bf0ba937f99dd0e30227c69f18b063e821f53ec
Sai <saisankeerthobulam@gmail.com>
2020-12-29 14:50:44 +0530

    issue fixed

--- src/components/home/NewHome.jsx
+++ src/components/home/NewHome.jsx
@@ -1,6 +1,6 @@
-import {useFocusEffect} from '@react-navigation/native';
+import { useFocusEffect } from '@react-navigation/native';
 import _find from 'lodash/find';
-import React, {useCallback, useState, useEffect} from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import {
   BackHandler,
   Dimensions,
@@ -21,22 +21,22 @@ import {
   contentSections,
 } from '../../../qbconfig';
 // import {Boy, Girl, MainImage, Men, Women} from '../../icons/Icons';
-import {colors} from '../../theme/colors';
-import {theme} from '../../theme/theme';
+import { colors } from '../../theme/colors';
+import { theme } from '../../theme/theme';
 import useAsyncStorage from '../customHooks/async';
-import {Loader} from '../Loader';
-import {ScreenNamesCustomer} from '../navigationController/ScreenNames';
+import { Loader } from '../Loader';
+import { ScreenNamesCustomer } from '../navigationController/ScreenNames';
 import CommonAlertView from '../UI/CommonAlertView';
 import CommonSearchHeader from '../UI/CommonSearchHeader';
 import axios from 'axios';
 import _pickBy from 'lodash/pickBy';
 import _orderBy from 'lodash/orderBy';
-import {useDebounce} from 'use-debounce';
-import {useIsFocused} from '@react-navigation/native';
-import {ifIphoneX} from 'react-native-iphone-x-helper';
-import {checkTokenExpired} from '../../utils/general';
+import { useDebounce } from 'use-debounce';
+import { useIsFocused } from '@react-navigation/native';
+import { ifIphoneX } from 'react-native-iphone-x-helper';
+import { checkTokenExpired } from '../../utils/general';
 
-const {width, height} = Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 
 // console.log(width / 2, 'width is.....');
 
@@ -205,7 +205,7 @@ const styles = StyleSheet.create({
   },
 });
 
-export const NewHome = ({route, navigation}) => {
+export const NewHome = ({ route, navigation }) => {
   const [showSortView, setShowSortView] = useState(false);
   const [wishlistLoading, setWishlistLoading] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
@@ -221,10 +221,10 @@ export const NewHome = ({route, navigation}) => {
   const [catsSubcatsLoading, setCatsSubcatsLoading] = useState(true);
   const [appContentErrorText, setAppContentErrorText] = useState('');
   const [catsSubcatsErrorText, setCatsSubcatsErrorText] = useState('');
-  const {storageItem: accessToken, tokenLoading} = useAsyncStorage(
+  const { storageItem: accessToken, tokenLoading } = useAsyncStorage(
     '@accessToken',
   );
-  const {CATS_SUBCATS, APP_CONTENT, CATALOG_ITEMS_AC} = restEndPoints;
+  const { CATS_SUBCATS, APP_CONTENT, CATALOG_ITEMS_AC } = restEndPoints;
   const [searchText, setSearchText] = useState('');
   const [debouncedText] = useDebounce(searchText, 500);
   const isFocused = useIsFocused();
@@ -270,7 +270,7 @@ export const NewHome = ({route, navigation}) => {
     const getCatsSubcats = async () => {
       try {
         await axios
-          .get(CATS_SUBCATS.URL, {headers: requestHeaders})
+          .get(CATS_SUBCATS.URL, { headers: requestHeaders })
           .then((apiResponse) => {
             setCatsSubcatsLoading(false);
             // console.log(apiResponse.data, 'cats subcats');
@@ -308,7 +308,7 @@ export const NewHome = ({route, navigation}) => {
     const getAppContent = async () => {
       try {
         await axios
-          .get(APP_CONTENT.URL, {headers: requestHeaders})
+          .get(APP_CONTENT.URL, { headers: requestHeaders })
           .then((apiResponse) => {
             setAppContentLoading(false);
             if (apiResponse.data.status === 'success') {
@@ -408,7 +408,7 @@ export const NewHome = ({route, navigation}) => {
     return (
       <TouchableOpacity
         activeOpacity={1}
-        style={[styles.brandRowStyles, {height: 200}]}
+        style={[styles.brandRowStyles, { height: 200, justifyContent: 'center' }]}
         onPress={() => {
           if (enableRedirection) {
             if (catalogId > 0) {
@@ -429,7 +429,7 @@ export const NewHome = ({route, navigation}) => {
           source={{
             uri: imageUrl,
           }}
-          style={{width: 200, height: 200}}
+          style={{ width: 155, height: 155, marginLeft: 20 }}
           resizeMode="contain"
         />
       </TouchableOpacity>
@@ -446,7 +446,7 @@ export const NewHome = ({route, navigation}) => {
         }}
         data={topBrands}
         horizontal={true}
-        renderItem={({item}) => renderRow(item)}
+        renderItem={({ item }) => renderRow(item)}
         keyExtractor={(item) => item.contentCode}
         removeClippedSubviews={true}
         showsHorizontalScrollIndicator={false}
@@ -489,7 +489,7 @@ export const NewHome = ({route, navigation}) => {
         }}
         data={hotSellers}
         horizontal={true}
-        renderItem={({item}) => renderRow(item)}
+        renderItem={({ item }) => renderRow(item)}
         keyExtractor={(item) => item.contentCode}
         removeClippedSubviews={false}
         showsHorizontalScrollIndicator={false}
@@ -512,7 +512,7 @@ export const NewHome = ({route, navigation}) => {
         }}
         data={orderedCategories}
         numColumns={2}
-        renderItem={({item}) => renderBrandRow(item)}
+        renderItem={({ item }) => renderBrandRow(item)}
         keyExtractor={(item) => item.categoryCode}
         removeClippedSubviews={false}
         showsHorizontalScrollIndicator={false}
@@ -570,11 +570,11 @@ export const NewHome = ({route, navigation}) => {
         }}>
         <Text style={styles.genderTextStyles}>{item.categoryName}</Text>
         <Image
-          source={{uri: imageUrl}}
-          style={{width: width / 2, height: width / 2}}
+          source={{ uri: imageUrl }}
+          style={{ width: width / 2, height: width / 2 }}
           resizeMode="stretch"
         />
-        <Text style={{...styles.viewTextStyles, paddingTop: 10}}>
+        <Text style={{ ...styles.viewTextStyles, paddingTop: 10 }}>
           View All Brands
         </Text>
       </TouchableOpacity>
@@ -583,7 +583,7 @@ export const NewHome = ({route, navigation}) => {
 
   const renderMainView = () => {
     return (
-      <View style={{width: '100%', height: 200}}>
+      <View style={{ width: '100%', height: 200 }}>
         {renderCarouselView()}
         {!appContentLoading && renderSliderDotView()}
       </View>
@@ -592,23 +592,23 @@ export const NewHome = ({route, navigation}) => {
 
   const renderCarouselView = () => {
     return (
-      <View style={{position: 'absolute', width: '100%', height: 200, top: 0}}>
+      <View style={{ position: 'absolute', width: '100%', height: 200, top: 0 }}>
         {appContentLoading ? (
           <Loader />
         ) : (
-          <Carousel
-            onSnapToItem={(slideIndex) => setSlideIndex(slideIndex)}
-            data={banners}
-            renderItem={renderSliderItem}
-            sliderWidth={width}
-            itemWidth={width}
-            loop
-            autoplay
-            autoplayDelay={3000}
-            autoplayInterval={3000}
-            layout="default"
-          />
-        )}
+            <Carousel
+              onSnapToItem={(slideIndex) => setSlideIndex(slideIndex)}
+              data={banners}
+              renderItem={renderSliderItem}
+              sliderWidth={width}
+              itemWidth={width}
+              loop
+              autoplay
+              autoplayDelay={3000}
+              autoplayInterval={3000}
+              layout="default"
+            />
+          )}
       </View>
     );
   };
@@ -639,7 +639,7 @@ export const NewHome = ({route, navigation}) => {
     />
   );
 
-  const renderSliderItem = ({item}) => {
+  const renderSliderItem = ({ item }) => {
     const imageUrl = encodeURI(
       `${cdnUrl}/${clientCode}/app-content/${item.imageName}`,
     );
@@ -649,7 +649,7 @@ export const NewHome = ({route, navigation}) => {
     // console.log('item in banners .....', item);
     return (
       <TouchableOpacity
-        style={{width: '100%', height: 200}}
+        style={{ width: '100%', height: 200 }}
         activeOpacity={1}
         onPress={() => {
           if (enableRedirection) {
@@ -667,9 +667,9 @@ export const NewHome = ({route, navigation}) => {
           }
         }}>
         <Image
-          source={{uri: imageUrl}}
+          source={{ uri: imageUrl }}
           PlaceholderContent={<Loader />}
-          style={{width: '100%', height: 200, position: 'absolute'}}
+          style={{ width: '100%', height: 200, position: 'absolute' }}
           resizeMode="stretch"
         />
       </TouchableOpacity>
@@ -701,7 +701,7 @@ export const NewHome = ({route, navigation}) => {
           backgroundColor: theme.colors.BLACK_WITH_OPACITY_5,
         }}
         data={searchData}
-        renderItem={({item}) => renderSearchRow(item)}
+        renderItem={({ item }) => renderSearchRow(item)}
         keyExtractor={(item) => item}
         removeClippedSubviews={false}
         showsHorizontalScrollIndicator={false}
